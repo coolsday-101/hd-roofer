c3ba057b7bd85786c01f5e932888a51ed442145c
xihai01 <xihai.luo01@gmail.com>
2025-07-16 16:59:10 -0400

    chore: remove financeit banner

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,6 +1,6 @@
-import * as React from "react";
-import { Link } from "gatsby";
-import TopBanner from "./TopBanner";
+import React from "react";
+//import { Link } from "gatsby";
+//import TopBanner from "./TopBanner";
 import NavigationHeader from "./NavigationHeader";
 import Footer from "./Footer";
 
@@ -8,7 +8,7 @@ const Layout = ({ pageTitle, children }) => {
   return (
     <>
       <header className="sticky top-0 z-10">
-        <TopBanner />
+        {/*<TopBanner />*/}
         <NavigationHeader />
       </header>
       <div className="font-archivo overflow-x-hidden">
